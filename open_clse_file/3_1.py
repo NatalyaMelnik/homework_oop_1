7e7f31ca35105b7be535e64a3b9b810472dc220c
NatalyaMelnik <natmel85@mail.ru>
2022-06-30 11:54:58 +0300

    add file1

---
+++ open_clse_file/3_1.py
@@ -0,0 +1,23 @@
+import os
+from pprint import pprint
+
+data = {}
+for filename in os.listdir("sorted"):
+    with open(os.path.join("sorted", filename), 'r', encoding='utf-8') as file_obj:
+        values = []
+
+        for line in file_obj:
+
+            values.append(line.strip())
+            data[filename] = values
+
+# pprint(data)
+
+def sorted_tuple(tuple_1):
+    return len(tuple_1[1])
+
+
+sorted_tuples = sorted(data.items(), key=sorted_tuple)
+
+for i in sorted_tuples:
+    print(i)
